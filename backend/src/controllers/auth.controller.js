da5be0a7fde690fc1320b057f872a06454ae180c
lavirox <lavanya23x@gmail.com>
2025-10-11 23:40:46 -0700

    feat(auth): add jwt token generation for real now

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
@@ -24,7 +25,16 @@ export const signup = async (req, res) => {
         });
 
         if(newUser){
-            //jwt token 
+            generateToken(newUser._id, res)
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                username: newUser.username,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+                createdAt: newUser.createdAt,
+                updatedAt: newUser.updatedAt,
+            });
         }
         else{
             return res.status(400).json({ message: "Invalid user data." });
@@ -34,7 +44,10 @@ export const signup = async (req, res) => {
 
         res.status(201).json({ message: "User created successfully." });
 
-    } catch (error) {}
+    } catch (error) {
+        console.log("Error in signup:", error.message);
+        res.status(500).json({ message: "Internal server error." });
+    }
 };
 export const login = (req, res) => {
     res.send("login route");
