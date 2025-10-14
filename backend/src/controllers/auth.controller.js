8db3d71128ccc0c8ab4e05173b3d2175709924a5
lavirox <lavanya23x@gmail.com>
2025-10-13 22:29:00 -0700

    feat(auth): add logout
    '
    
    '

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -54,9 +54,39 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: "Internal server error." });
     }
 };
-export const login = (req, res) => {
-    res.send("login route");
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email})
+
+        if(!user){
+            return res.status(400).json({ message: "Invalid email or password." });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid email or password." });
+        }
+        generateToken(user._id, res)
+        res.status(200).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
+    } catch (error) {
+        console.log("Error in login:", error.message);
+        res.status(500).json({ message: "Internal server error." });
+    }
 };
 export const logout = (req, res) => {
-    res.send("logout route");
+    try{
+        res.cookie("jwt", "", {maxAge: 0})
+        res.status(200).json({ message: "Logged out successfully." });
+    }
+    catch (error) {
+        console.log("Error in logout:", error.message);
+        res.status(500).json({ message: "Internal server error." });
+    }
 };
